a3f29e6312317cb22e875e3ff5748760d8b7d130
broxen <paul@broxen.net>
2021-10-04 18:10:50 -0700

    Move to not-found
    Not found is the correct page for this addition

--- client/components/new-page.vue
+++ client/components/new-page.vue
@@ -11,9 +11,6 @@
         v-btn.mt-5(color='purple lighten-3', href='javascript:window.history.go(-1);', outlined)
           v-icon(left) mdi-arrow-left
           span {{ $t('newpage.goback') }}
-        v-btn.mt-5(:href='`/login/`', x-large)
-          v-icon(left) mdi-login
-          span {{ $t('newpage.login') }}
 </template>
 
 <script>
@@ -28,7 +25,7 @@ export default {
       type: String,
       default: 'home'
     }
-  },
+  }, 
   data() {
     return { }
   }
